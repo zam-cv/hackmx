f7d52492301c5cb531eb4ea6c865dadab07d3942
Sebastian <A01749694@tec.mx>
2024-06-27 21:01:13 +0000

    Context on App.tsx

--- page/src/SettingsContext.tsx
+++ page/src/SettingsContext.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 export const SETTINGS_CONTEXT = React.createContext({})
 
-export default function SETTINGS_CONTEXT_PROVIDER({children}: any){
+export default function SettingsContextProvider({children}: any){
   const [dark, setDark] = useState("dark")
 
   return (
