468436a8aa214815b83cd9cafed86f6462f611a8
Sebastian <A01749694@tec.mx>
2024-07-03 18:19:55 +0000

    Landing Page

--- page/src/pages/LandingPage.tsx
+++ page/src/pages/LandingPage.tsx
@@ -9,9 +9,12 @@ export default function LandingPage() {
   return (
     <div className={theme}>
       <h1>Landing Page</h1>
+      
       <Timer targetDate="2024-10-15T00:00:00" />
 
       <ThemeSwitch />
+
+      <footer>Tecnologico de Monterrey 2024</footer>
     </div>
   );
 }
