1c6cc698e1f19d61669c53f60c95aa2db3655451
Alberto Velazquez <117140491+zam-cv@users.noreply.github.com>
2024-07-06 15:56:32 -0600

    init: astro

--- page/src/hooks/useAuth.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { AuthContext, AuthContextType } from "../contexts/AuthContext";
-
-export function useAuth(): AuthContextType {
-  return useContext(AuthContext);
-}
--- page/src/hooks/useSettings.tsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { SettingsContext, SettingsContextType } from "../contexts/SettingsContext";
-
-export default function useSettings(): SettingsContextType {
-  return useContext(SettingsContext)
-}
