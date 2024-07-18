51913bc2b9be7b6d7122774ce6ef5ed1f76849fd
A01749697 <A01749697@tec.mx>
2024-07-18 13:32:34 -0600

    Add users table to database

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -28,3 +28,25 @@ diesel::allow_tables_to_appear_in_same_query!(
     admins,
     users,
 );
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use crate::models::types::exports::*;
+
+    users (id) {
+        id -> Integer,
+        first_name -> Varchar,
+        #[max_length = 50]
+        last_name -> Varchar,
+        #[max_length = 100]
+        email -> Varchar,
+        #[max_length = 50]
+        password -> Varchar,
+        #[max_length = 50]
+        phone_number -> Varchar,
+        #[max_length = 14]
+        terms_accepted -> Bool,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
